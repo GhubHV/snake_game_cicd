4618a22e5cf630dd7dc1e08ea28564577f1fa817
GhubHV <heliasvec@gmail.com>
2024-06-29 18:41:31 +0200

    essai2

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snake Game</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+
+<body>
+    <div class="container">
+        <h1>Snake Game</h1>
+        <table id="numberTable">
+            <tbody>
+                <script src="script.js"></script>
+            </tbody>
+        </table>
+    </div>
+    <footer>Réalisé par Mael OLIVEIRA, Théo HOFFMANN et Hélias VECELLIO - <?php echo date('Y'); ?></footer>
+
+</body>
+
+</html>
