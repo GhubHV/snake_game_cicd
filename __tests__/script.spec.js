eb8234e0a67e4029510baa32ace5b464d43ff1c5
Théo Hoffmann <thebosq99@gmail.com>
2024-07-07 21:43:13 +0200

    ✅ Ajout  de test complémentaire  concernant la fonction calculerPiegesBonus

--- __tests__/script.spec.js
+++ __tests__/script.spec.js
@@ -29,13 +29,8 @@ global.window = dom.window;
 const script = require('../script');
 
 describe('Tests for script.js', () => {
-    beforeEach(() => {
-        // Réinitialiser les pièges et bonus avant chaque test
-        script.calculerPiegesBonus();
-    });
 
     test('calculerPiegesBonus génère des pièges et bonus', () => {
-        // On appelle à nouveau la fonction pour obtenir les valeurs après qu'elles aient été réinitialisées
         const { piege1, piege2, piege3, bonus1, bonus2 } = script.calculerPiegesBonus();
         expect(piege1).toBeGreaterThan(1);
         expect(piege1).toBeLessThanOrEqual(49);
@@ -47,11 +42,17 @@ describe('Tests for script.js', () => {
         expect(bonus1).toBeLessThanOrEqual(29);
         expect(bonus2).toBeGreaterThan(1);
         expect(bonus2).toBeLessThanOrEqual(29);
+
+        expect(piege1).not.toBe(piege2);
+        expect(piege1).not.toBe(piege3);
+        expect(piege2).not.toBe(piege3);
+
+        expect(bonus1).not.toBe(bonus2);
     });
 
     test('dessinerPlateau doit retourner un plateau avec la position des joueurs', () => {
         const positionJoueur1 = 1;
-        const positionJoueur2 = 2;
+        const positionJoueur2 = 1;
         script.dessinerPlateau(positionJoueur1, positionJoueur2);
         const table = document.querySelector('#numberTable tbody');
         expect(table.children.length).toBe(5);
@@ -67,5 +68,12 @@ describe('Tests for script.js', () => {
         const tirage = 4;
         positionJoueur1 += tirage;
         expect(positionJoueur1).toBe(5);
+        expect(positionJoueur1).not.toBe(7);
+
+        let positionJoueur2 = 1;
+        const tirage2 = 5;
+        positionJoueur2 += tirage2;
+        expect(positionJoueur2).toBe(6);
+        expect(positionJoueur2).not.toBe(7);
     });
 });
